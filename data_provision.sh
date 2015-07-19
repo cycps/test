424c911b8f3f0cd5e010399891d44351544b4981
Ryan Goodfellow <rgoodfel@isi.edu>
2015-07-18 18:23:22 -0700

    pg_setup

--- data_provision.sh
+++ data_provision.sh
@@ -5,6 +5,9 @@ sudo sed -i 's/http:\/\/us.archive.ubuntu.com/http:\/\/ubuntu.eecs.wsu.edu/' /et
 sudo sed -i 's/http:\/\/security.ubuntu.com/http:\/\/ubuntu.eecs.wsu.edu/' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y vim tmux postgresql postgresql-contrib postgresql-doc
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.4/main/postgresql.conf
+sudo bash -c " echo 'host    all             all             192.168.1.0/8           trust' >> /etc/postgresql/9.4/main/pg_hba.conf "
+sudo invoke-rc.d postgresql restart
 echo "making cyp dir"
 mkdir -p $HOME/.cypress/log
 echo "dbscript"
--- svc_provision.sh
+++ svc_provision.sh
@@ -4,12 +4,13 @@ sudo chown -R vagrant:vagrant /home/vagrant/go
 sudo sed -i 's/http:\/\/us.archive.ubuntu.com/http:\/\/ubuntu.eecs.wsu.edu/' /etc/apt/sources.list
 sudo sed -i 's/http:\/\/security.ubuntu.com/http:\/\/ubuntu.eecs.wsu.edu/' /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y vim tmux golang
+sudo apt-get install -y vim tmux git golang
 echo "installing go"
 echo "export GOPATH=/home/vagrant/go" >> /home/vagrant/.bashrc
 echo "export PATH=$PATH:/home/vagrant/go/bin" >> /home/vagrant/.bashrc
 source /home/vagrant/.bashrc  
 echo "installing addie"
+GOPATH=/home/vagrant/go go get github.com/lib/pq
 GOPATH=/home/vagrant/go go install github.com/cycps/addie
 echo "running addie"
 PATH=$PATH:/home/vagrant/go/bin addie &
