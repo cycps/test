f8a56ebea965806167aa9ab8403854bb38b732c7
Ryan Goodfellow <rgoodfel@isi.edu>
2015-08-13 11:35:32 -0700

    svc gets sim

--- svc_provision.sh
+++ svc_provision.sh
@@ -19,3 +19,38 @@ GOPATH=/home/vagrant/go go get github.com/lib/pq github.com/julienschmidt/httpro
 GOPATH=/home/vagrant/go go install github.com/cycps/addie/go-addie
 #echo "running design"
 #PATH=$PATH:/home/vagrant/go/bin design &
+
+mkdir ~/lib
+cd ~/lib
+cp ~/.cypress/sim/lib/* .
+
+#mpich
+tar xzf mpich-3.1.4.tar.gz
+cd mpich-3.1.4
+./configure --disable-fortran
+make
+sudo make install
+cd ..
+
+sudo apt-get install -y libbz2-dev
+tar xzf boost_1_58_0.tar.gz
+cd boost_1_58_0
+./bootstrap.sh toolset=clang cxxflags="-std=c++1y -stdlib=libc++" linkflags="-stdlib=libc++"
+./b2 toolset=clang cxxflags="-std=c++1y -stdlib=libc++" linkflags="-stdlib=libc++" -j 4 stage release --without-python
+sudo ./b2 install toolset=clang cxxflags="-std=c++1y -stdlib=libc++" linkflags="-stdlib=libc++" --without-python
+cd ..
+
+tar xzf sundials-2.5.0.tar.gz
+cd sundials-2.5.0
+./configure --enable-shared
+make
+sudo make install
+
+cd ~/lib
+git clone https://github.com/rcgoodfellow/RyMPI.git
+cd RyMPI
+mkdir build
+cd build
+cmake .. -G Ninja
+ninja
+sudo ninja install
