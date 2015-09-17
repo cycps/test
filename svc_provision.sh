2d878dc8e9a271a2fc0922dcec36db9c5b1985a9
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-17 10:27:40 -0700

    bind9utils on svc

--- svc_provision.sh
+++ svc_provision.sh
@@ -9,6 +9,7 @@ sudo sed -i 's/http:\/\/security.ubuntu.com/http:\/\/ubuntu.eecs.wsu.edu/' /etc/
 sudo apt-get update
 sudo apt-get install -y vim tmux git golang
 sudo apt-get install -y build-essential clang cmake ninja-build libc++1 libc++abi1 libc++-dev
+sudo apt-get install -y bind9utils
 echo "installing go"
 echo "export GOPATH=/home/vagrant/go" >> /home/vagrant/.bashrc
 echo "export PATH=$PATH:/home/vagrant/go/bin" >> /home/vagrant/.bashrc
